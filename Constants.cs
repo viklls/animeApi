151a20f47df6b5cae2b375ee1d474a58a7dc8cd3
viktoria <162178136+viklls@users.noreply.github.com>
2024-06-07 01:45:47 +0300

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coursework
+{
+    internal class Constants
+    {
+        public static string Address = "https://anime-manga-and-novels-api.p.rapidapi.com/anime/";
+        public static string ApiKey = "9b27f8d856mshb282a6ac2a63b66p17b619jsnf8c4dae25f31";
+        public static string ApiHost = "anime-manga-and-novels-api.p.rapidapi.com";
+        public static string AAddress = "https://anime-manga-and-novels-api.p.rapidapi.com/novels?pageSize=2&page=1";
+        public static string AApiKey = "9b27f8d856mshb282a6ac2a63b66p17b619jsnf8c4dae25f31";
+        public static string AApiHost = "anime-manga-and-novels-api.p.rapidapi.com";
+    }
+}
